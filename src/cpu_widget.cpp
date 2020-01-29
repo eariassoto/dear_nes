266f5f5b038cb7f9e8790f1449b9a5ae666f2f00
Emmanuel Arias Soto <ariassotoemmanuel@gmail.com>
2020-01-29 22:26:16 +0100

    Add debug buttons

--- src/cpu_widget.cpp
+++ src/cpu_widget.cpp
@@ -39,6 +39,34 @@ void CpuWidget::Render() {
     ImGui::Text("Y: $%02x [%d]", regY, regY);
     ImGui::Text("Stack Pointer: $%04x", m_Cpu.GetStackPointer());
     ImGui::End();
+
+    ImGui::Begin("CPU controls");
+    ImGui::SetWindowSize({200, 60});
+    ImGui::SameLine();
+    bool step = ImGui::Button("Step");
+    if (step) {
+        do {
+            m_Cpu.Clock();
+        } while (!m_Cpu.InstructionComplete());
+    }
+    ImGui::SameLine();
+    bool reset = ImGui::Button("Reset");
+	if (reset)
+	{
+        m_Cpu.Reset();
+	}
+    ImGui::SameLine();
+    bool irq = ImGui::Button("IRQ");
+	if (irq)
+	{
+        m_Cpu.InterruptRequest();
+	}
+    ImGui::SameLine();
+    bool nmi = ImGui::Button("NMI");
+    if (nmi) {
+        m_Cpu.NonMaskableInterrupt();
+    }
+    ImGui::End();
 }
 
 }  // namespace cpuemulator
