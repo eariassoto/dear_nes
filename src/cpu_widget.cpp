fcf647307281aa05815e5470e26a7b7f7fc9222f
Emmanuel Arias Soto <ariassotoemmanuel@gmail.com>
2020-02-12 21:02:46 +0100

    remove debug controls from CPU widget

--- src/cpu_widget.cpp
+++ src/cpu_widget.cpp
@@ -11,7 +11,7 @@ CpuWidget::CpuWidget(Cpu& cpu)
 
 void CpuWidget::Render() {
     ImGui::Begin("CPU registers");
-    ImGui::SetWindowSize({280, 160});
+    ImGui::SetWindowSize({ 280, 160 });
 
     ImGui::Text("Status: ");
     ImGui::SameLine();
@@ -41,34 +41,5 @@ void CpuWidget::Render() {
     ImGui::Text("PC: $%04X", m_Cpu.m_ProgramCounter);
     ImGui::Text("Instruction at PC: %s", m_Cpu.GetInstructionString(m_Cpu.m_ProgramCounter).c_str());
     ImGui::End();
-
-    ImGui::Begin("CPU controls");
-    ImGui::SetWindowSize({200, 60});
-    ImGui::SameLine();
-    bool step = ImGui::Button("Step");
-    if (step) {
-        do {
-            m_Cpu.Clock();
-        } while (!m_Cpu.InstructionComplete());
-    }
-    ImGui::SameLine();
-    bool reset = ImGui::Button("Reset");
-	if (reset)
-	{
-        m_Cpu.Reset();
-	}
-    ImGui::SameLine();
-    bool irq = ImGui::Button("IRQ");
-	if (irq)
-	{
-        m_Cpu.InterruptRequest();
-	}
-    ImGui::SameLine();
-    bool nmi = ImGui::Button("NMI");
-    if (nmi) {
-        m_Cpu.NonMaskableInterrupt();
-    }
-    ImGui::End();
 }
-
 }  // namespace cpuemulator
