0d82c5ef94872c0d11bf165da3dee30d53fc6f00
Emmanuel Arias Soto <ariassotoemmanuel@gmail.com>
2021-03-22 14:11:20 +0100

    Refactor graphics manager

---
+++ src/include/imgui_window_manager.h
@@ -0,0 +1,33 @@
+// Copyright (c) 2020-2021 Emmanuel Arias
+#pragma once
+#include <vector>
+
+class GLFWwindow;
+class BaseWidget;
+
+class ImGuiWindowManager {
+   public:
+    ImGuiWindowManager();
+    ~ImGuiWindowManager();
+
+    bool CreateWindow();
+    bool ShouldClose();
+    void HandleEvents();
+
+    virtual void RegisterWidgets() = 0;
+    virtual void ProcessInput(GLFWwindow* window) = 0;
+
+    void Update(float deltaTime);
+    void Render();
+
+    void DestroyWindow();
+
+   protected:
+    BaseWidget* AddWidget(BaseWidget* newWidget);
+
+   private:
+    GLFWwindow* m_Window = nullptr;
+    std::vector<BaseWidget*> m_Widgets;
+
+    void ShowDockSpace(bool* p_open);
+};
