80fccddc185c097f4bc0c3c51e8093c4e432a54b
Emmanuel Arias <ariassotoemmanuel@gmail.com>
2021-03-24 00:39:35 +0100

    fix warning in char* array

--- src/include/controller_widget.h
+++ src/include/controller_widget.h
@@ -1,6 +1,7 @@
 // Copyright (c) 2020-2021 Emmanuel Arias
 #pragma once
 #include <string>
+
 #include "include/dearnes_base_widget.h"
 
 class ControllerWidget : public DearNESBaseWidget {
@@ -15,14 +16,11 @@ class ControllerWidget : public DearNESBaseWidget {
     std::string m_WindowName;
 
     static constexpr size_t NUM_BUTTONS = 8;
-    static constexpr char* m_ButtonNames[NUM_BUTTONS] = {
-        "Up", "Down", "Left", "Right",
-                                   "A",  "B",    "Select", "Start"};
-    static constexpr char* m_ButtonBinds[NUM_BUTTONS] = {
-        "Arrow Up", "Arrow Down", "Arrow Left",
-                                   "Arrow Right", "S",          "A",
-                                   "Q",           "W"};
+    static constexpr const char* m_ButtonNames[NUM_BUTTONS] = {
+        "Up", "Down", "Left", "Right", "A", "B", "Select", "Start"};
+    static constexpr const char* m_ButtonBinds[NUM_BUTTONS] = {
+        "Arrow Up", "Arrow Down", "Arrow Left", "Arrow Right",
+        "S",        "A",          "Q",          "W"};
     static constexpr uint8_t m_ButtonMasks[NUM_BUTTONS] = {
-        0x08, 0x04, 0x02, 0x01,
-                                     0x80, 0x40, 0x20, 0x10};
+        0x08, 0x04, 0x02, 0x01, 0x80, 0x40, 0x20, 0x10};
 };
