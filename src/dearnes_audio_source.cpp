edff6f7ec01bb6fa58e8fa7ff95e0f9ac331c2e8
Emmanuel Arias Soto <ariassotoemmanuel@gmail.com>
2021-03-28 23:30:59 +0200

    Add sine wave test

--- src/dearnes_audio_source.cpp
+++ src/dearnes_audio_source.cpp
@@ -1,18 +1,25 @@
 // Copyright (c) 2021 Emmanuel Arias
 #include "include/dearnes_audio_source.h"
 
+#include <soloud_misc.h>
+
 #include "dear_nes_lib/nes.h"
 
 DearNESAudioSourceInstance::DearNESAudioSourceInstance(
     DearNESAudioSource *aParent)
-    : m_Parent{aParent} {}
+    : m_Parent{aParent}, mOffset{0}, mFreq{aParent->mFreq}, mT{0} {}
 
 unsigned int DearNESAudioSourceInstance::getAudio(float *aBuffer,
                                                   unsigned int aSamplesToRead,
                                                   unsigned int aBufferSize) {
-    dearnes::Nes *nesPtr = m_Parent->m_NesPtr;
+    int waveform = m_Parent->mWaveform;
+    float d = 1.0f / mSamplerate;
     for (unsigned int i = 0; i < aSamplesToRead; i++) {
-        aBuffer[i] = 0.f;
+        aBuffer[i] = SoLoud::Misc::generateWaveform(
+                         waveform, fmod(mFreq * (float)mOffset, 1.0f)) *
+                     m_Parent->mADSR.val(mT, 10000000000000.0f);
+        mOffset++;
+        mT += d;
     }
     return aSamplesToRead;
 }
@@ -22,7 +29,9 @@ bool DearNESAudioSourceInstance::hasEnded() {
     return 0;
 }
 
-DearNESAudioSource::DearNESAudioSource() { mBaseSamplerate = 44100.f; }
+DearNESAudioSource::DearNESAudioSource()
+    : mWaveform{SoLoud::Soloud::WAVE_SIN},
+      mFreq{(float)(440.f / mBaseSamplerate)} {}
 
 DearNESAudioSource::~DearNESAudioSource() { stop(); }
 
--- src/include/dearnes_audio_source.h
+++ src/include/dearnes_audio_source.h
@@ -7,6 +7,42 @@ class Nes;
 };
 class DearNESAudioSource;
 
+class ADSR {
+   public:
+    float mA, mD, mS, mR;
+
+    ADSR() {
+        mA = 0.0f;
+        mD = 0.0f;
+        mS = 1.0f;
+        mR = 0.0f;
+    }
+
+    ADSR(float aA, float aD, float aS, float aR) {
+        mA = aA;
+        mD = aD;
+        mS = aS;
+        mR = aR;
+    }
+
+    float val(float aT, float aRelTime) {
+        if (aT < mA) {
+            return aT / mA;
+        }
+        aT -= mA;
+        if (aT < mD) {
+            return 1.0f - ((aT / mD)) * (1.0f - mS);
+        }
+        aT -= mD;
+        if (aT < aRelTime) return mS;
+        aT -= aRelTime;
+        if (aT >= mR) {
+            return 0.0f;
+        }
+        return (1.0f - aT / mR) * mS;
+    }
+};
+
 class DearNESAudioSourceInstance : public SoLoud::AudioSourceInstance {
    public:
     DearNESAudioSourceInstance(DearNESAudioSource *aParent);
@@ -16,6 +52,9 @@ class DearNESAudioSourceInstance : public SoLoud::AudioSourceInstance {
 
    private:
     DearNESAudioSource *m_Parent = nullptr;
+    float mFreq;
+    int mOffset;
+    float mT;
 };
 
 class DearNESAudioSource : public SoLoud::AudioSource {
@@ -26,4 +65,8 @@ class DearNESAudioSource : public SoLoud::AudioSource {
     virtual SoLoud::AudioSourceInstance *createInstance();
 
     dearnes::Nes *m_NesPtr = nullptr;
+
+    ADSR mADSR;
+    float mFreq;
+    int mWaveform;
 };
